6ba84d17932330dd754b9ba91d2fb1e22d58f765
tfjiao <jiaotengfei1016@163.com>
2021-04-06 23:05:51 +0800

    基本操作封装更新

--- Common/base.py
+++ Common/base.py
@@ -1,7 +1,13 @@
 # --*--coding=utf-8--*--
+from Common.log import FrameLog
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from Common.log import FrameLog
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.remote.webelement import WebElement
+import logging
 
 
 class Base:
@@ -9,130 +15,181 @@ class Base:
 
     def __init__(self, driver, log):
         self.driver = driver
-        self.log = FrameLog().log()
 
-    def findele(self, *args):
+    def id(self, element):
+        return self.driver.find_element_by_id(element)
+
+    def findele(self, *element):
         """
-        定位元素,单星号参数代表此处接受任意多个非关键字参数
-        :param args:
+        压包，和解包，示例：findele(By.Id, "xxx")
+        """
+        return self.driver.find_element(*element)
+
+    def findeles(self, *element):
+        """
+        压包，和解包，例如findele(By.Id, "xxx"),返回是一个许多个元素
+        :param element:
         :return:
         """
-        return self.driver.find_element(*args)
+        return self.driver.find_elements(*element)
 
-    # 对元素click
-    def click(self, *args):
-        return self.findele(*args).click()
+    def clear(self, *element):
+        # 清空要输入的内容，一般用于输入前置
+        return self.driver.findele(*element).clear()
+        
+    def click(self, *element):
+        """
+        点击元素，示例：click(By.Xpath, '[xxx]')
+        """
+        return self.driver.findele(*element).click()
 
-    # 发送信息
-    def sendkey(self, args, value):
-        self.findele(*args).send_keys(value)
+    def sendkey(self, value, *element):
+        """
+        给输入框发送内容，示例：sendkey("tfjiao", By.Xpath, '[xxx]')
+        """
+        self.findele(*element).send_keys(value)
 
-    # 调用js方法
+    def move_offset_click(self, canvas, *element):
+        """
+        移动到画布这个元素上，移动到某个位置点，并进行点击 >>> 用于画布桑画形状
+        示例：move_offset_click(By.Xpath, '[xxx]', 200, 300)
+        200是x坐标，300是y坐标。具体参考move_by_offset的源码
+        """
+        return ActionChains(self.driver).move_to_element(canvas).move_by_offset(*element).release().click().perform()
+
+    def move_offset_click_double(self, canvas, *elenment):
+        """
+        移动到画布这个元素上，移动到某个位置点，并进行双击，跟move_offset_click方法类似 >>> 用于画布上双击结束绘画
+        """
+        return ActionChains(self.driver).move_to_element(canvas).move_by_offset(*elenment).release().double_click().perform()
+
+    def move_element_click(self, *element):
+        """
+        移动到元素上，并点击。示例：move_element_click(By.Xpath, '[xxx]')
+        """
+        return ActionChains(self.driver).move_to_element(self.findele(*element)).release().click().perform()
+
+    def move_element_offset_click(self, xoffset, yoffset, *element):
+        """
+        移动到元素的左侧位置，示例：move_element_offset_click(100, 200, By.Id, "xxx")
+        """
+        return ActionChains(self.driver).move_to_element(self.findele(*element), xoffset, yoffset).click().perform()
+
+    def select_value(self, id, value):
+        """
+        选择下拉框的元素，id是下拉框的id值，value是下拉框元素的value属性值
+        注意：下拉框有时候开发会把display设置为none,这里需要js脚本打开改变属性值，后续js脚本会说到
+        """
+        return Select(self.findele(By.ID, id)).select_by_value(value)
+
+    def select_text(self, id, text):
+        """
+        选择下拉框的元素，id是下拉框的id,text是下拉框元素显示在页面的内容
+        注意：下拉框有时候开发会把display设置为none,需要js脚本处理
+        """
+        return Select(self.findele(By.ID, id)).select_by_visible_text(text)
+
+    def select_index(self, id, index):
+        """
+        选择下拉框的元素，id是下拉框的id,index是索引值，从0开始
+        注意：下拉框有时候开发会把display设置为none,需要js脚本处理
+        """
+        return Select(self.findele(By.ID, id)).select_by_index(index)
+
+    def js(self):
+        """
+        关于js的用法，document.querySelectorAll('css语法')基本满足了所有需要
+        selenium_js定位详解：https://blog.csdn.net/weixin_45451320/article/details/115104455
+        selenium_css定位详解：https://blog.csdn.net/weixin_45451320/article/details/115101192
+        """
+        return self.driver.execute_script('document.querySelectorAll("css语法")[1].click()')
+
     def js(self, str):
-        self.driver.execute_script(str)
+        return self.driver.execute_script(str)
 
-    def dddd(self, *args):
+    def click_js(self, *element):
+        """
+        js语法点击
+        """
+        return self.driver.execute_script("argument[0].click();", self.findele(*element))
+
+    def switchframe(self, element):
+        """
+        切换框架
+        """
+        return self.driver.switch_to.frame(element)
+
+    def wait_click(self, *element):
+        """
+        每0.5秒检查一次元素，元素能被点击，则执行。
+        示例：wait_click(By.Xpath, '[xxx]')
+        """
+        return WebDriverWait(self.driver, 20, 0.5).until(EC.element_to_be_clickable(element))
+
+    def wait_located(self, *element):
         """
         检查元素是否存在于页面和可见。可见性意味着不仅显示元素,但其高度和宽度也大于0。定位器-用于查找元素,
         一旦WebElement定位并可见，返回它
-        :param args:
-        :return:
         """
-        return WebDriverWait(self.driver, 20, 0.5).until(EC.visibility_of_element_located())
+        return WebDriverWait(self.driver, 20, 0.5).until(EC.visibility_of_element_located(element))
 
-    def aaaaaaaa(self,*args):
+    def wait_alert(self,*element):
         """
         检查是否有弹窗出现
         如果点击某个操作有弹框，用这个更快。
-        :param args:
-        :return:
         """
-        return WebDriverWait(self.driver, 20, 0.5).until(EC.alert_is_present())
+        return WebDriverWait(self.driver, 20, 0.5).until(EC.alert_is_present(element))
 
-    # 调用不要输入元组。*args已经打包了
-    def dominance_clickable(self, *args):
-        """
-        检查元素的期望是可见的并已启用，以便您可以单击它
-        点击元素之前带上这个，会更快。
-        :param args:
-        :return:
-        """
-        return WebDriverWait(self.driver, 20, 0.5).until(EC.element_to_be_clickable(args))
-
-    def aaa1(self,*args):
+    def wait_title(self,*element):
         """
         检查标题是否包含区分大小写的子字符串。title是期望的title片段,当标题匹配时返回True，否则返回False
         切换浏览器界面的title时候带上这个，会更快。
-        :return:
         """
-        return WebDriverWait(self.driver, 20, 0.5).until(EC.title_contains(args))
+        return WebDriverWait(self.driver, 20, 0.5).until(EC.title_contains(element))
 
-    def aaaa2(self, *args):
+    def wait_frame(self, *element):
         """
         期望检查给定帧是否可用于,切换到。 如果框架可用，则将给定的驱动程序切换到指定的帧。
         切换框架时候，用它更快。
-        :param args:
-        :return:
         """
-        return WebDriverWait(self.driver, 20, 0.5).until(EC.frame_to_be_available_and_switch_to_it(args))
+        return WebDriverWait(self.driver, 20, 0.5).until(EC.frame_to_be_available_and_switch_to_it(element))
 
-    def aaaaa2(self,*args):
+    def wait_text_present(self,*element):
         """
         期望检查给定文本是否存在于指定的元素。
         如果你定位text的文本并点击它，使用它更快
-        :param args:
-        :return:
         """
-        return WebDriverWait(self.driver, 20, 0.5).until(EC.text_to_be_present_in_element)
+        return WebDriverWait(self.driver, 20, 0.5).until(EC.text_to_be_present_in_element(element))
 
-    def aaa(self, *args):
+    def wait_visibility_located(self, *element):
         """
         检查DOM上是否存在元素的期望值。一页的长度。这并不一定意味着元素是可见的。
-        这个研究中
-        :return:
         """
-        return WebDriverWait(self.driver,20,0.5).until(EC.presence_of_element_located(args))
+        return WebDriverWait(self.driver,20,0.5).until(EC.visibility_of_element_located(element))
 
-    def bbb(self, *args):
+    def wait_selected(self, *element):
         """
         选择检查选择的期望。元素是WebElement对象
-        不是很明白
-        :param args:
-        :return:
         """
-        return WebDriverWait(self.driver, 20, 0.5).until(EC.element_to_be_selected(args))
+        return WebDriverWait(self.driver, 20, 0.5).until(EC.element_to_be_selected(element))
 
+    def displayed(self, *element):
+        """
+        检查元素是否可见，可见则通过
+        """
+        return self.driver.findele(*element).is_displayed()
 
-
-
+    def enabled(self, *element):
+        return self.driver.findele(*element).is_enabled()
 
     def url(self):
         return self.driver.current_url
 
-    # 后退
     def back(self):
         self.driver.back()
 
-    # 前进
     def forward(self):
         self.driver.forward()
 
-    # 退出
     def quit(self):
         self.driver.quit()
-
-    # # 函数id将返回按照id属性来定位元素的语句
-    # def id(element):
-    #     return driver.find_element_by_id(element)
-    #
-    # # 函数css将返回css selector方式来定位元素的语句
-    # def css(element):
-    #     return driver.find_element_by_css_selector(element)
-    #
-    # # 函数xpath将返回xpath方式来定位元素的语句
-    # def xpath(element):
-    #     return driver.find_element_by_xpath(element)
-    #
-    # # 函数js通过selenium来执行javascript语句
-    # def js(element):
-    #     driver.execute_script(f"document.getElementById(f'{element}').removeAttribute('readonly')")
