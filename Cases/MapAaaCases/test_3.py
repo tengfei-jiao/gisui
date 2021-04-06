59f4164ceb796718d51e0fedea698717c6c54f25
tfjiao <jiaotengfei1016@163.com>
2021-04-06 21:50:01 +0800

    更新框架

--- Cases/MapAaaCases/test_3.py
+++
@@ -1,4 +0,0 @@
-
-def test_assertion():
-    assert [1, 2, 3] == [1, 2, 4], "left is [1,2,3], right is [1,2,4]"
-
---
+++ script/test_01.py
@@ -0,0 +1,12 @@
+
+import sys
+
+# sys.argv[1]表示传入的第一个参数
+a = sys.argv[1]
+b = sys.argv[2]
+
+
+print(f"{a} come on ")
+print(f"{b} shi dashagua")
+# sys.argv[0]表示脚本名
+print(sys.argv[0])
